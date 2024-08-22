d900c102d672143f2f3ae0289c66dd8816442574
ali221022 <shohjahon.juraev.4496@mail.ru>
2024-08-22 11:50:08 +0900

    BRR: feat: mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,46 +1,33 @@
-console.log("Xush kelibsiz Shohjahon!");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
+let db;
+const connectionString = "mongodb+srv://ali221022:ali221022!@cluster0.dhqnc.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0"
 
-// 1 Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+mongodb.connect(
+    connectionString,
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+},
+ (err, client) =>  {
+   if(err) console.log("ERROR on connection MongoDB");
+   else {
+    console.log("MongoDB connection succeed");
+    module.exports = client;
+    const app = require("./app");
+    const server = http.createServer(app);
+    let PORT = 3000;
+    server.listen(PORT, function () {
+    console.log(
+        `The server is running successfully on port localhost:3000, http://localhost:3000`
 
-// 2: Session code
+         );
+       }
+     );
+   }
+ }
+);
 
-// 3 Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
 
-// 4 Routing code
-app.post("/create-item", (req, res) => {
-  //  console.log(req.body);
-  //  res.json({test:"success"});
-  // TODO: code with db here
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(`The server is running successfully on port localhost:3000, http//localhost:3000`);
-});
+// mongodb+srv://ali221022:ali221022!@cluster0.dhqnc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
